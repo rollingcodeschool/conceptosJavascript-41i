6c6c133b50ffe3346b0dcffcee4945cfe17501bc
Emilse Arias <emilsearias@gmail.com>
2023-02-22 18:47:23 -0300

    creamos algunas variables

---
+++ js/1-variables.js
@@ -0,0 +1,22 @@
+//Esto es un comentario de una linea
+
+/*
+Es un comentario
+de multiples 
+lineas
+*/
+
+// crear o declarar una variable (var - let - const)
+
+let producto = 'ipad pro';
+let precio = 700;
+let luzEncendida = false;
+
+// mostrar en el documento html
+document.write('Hola mundo');
+document.write('<br>');
+document.write(precio * 380);
+
+// mostrar en la consola
+console.log(precio * 380);
+
