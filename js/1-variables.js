8bd3f4411502f80f272438380d63886b9cf7e2c5
Emilse Arias <emilsearias@gmail.com>
2023-02-27 17:27:59 -0300

    agregamos back ticks la ventana de prompt

--- js/1-variables.js
+++ js/1-variables.js
@@ -13,10 +13,22 @@ let precio = 700;
 let luzEncendida = false;
 
 // mostrar en el documento html
-document.write('Hola mundo');
+document.write('<h1>Hola mundo</h1>');
 document.write('<br>');
-document.write(precio * 380);
+document.write('Precio: $'+ precio * 380);
+document.write('<p>Precio: $'+ (precio + 5)+'</p>');
+document.write(`<p>Precio: $ ${precio + 5} </p>`);
+
 
 // mostrar en la consola
 console.log(precio * 380);
+
+let vacio = null;
+// mostrar una ventana emergente
+alert('Hola mundo');
+
+// parseInt('numero')
+let numero = parseFloat(prompt('ingrese un numero'));
+console.log(numero);
+console.log(typeof(numero));
 
