ae18aae3544967805c68146fe8ef4e83b849c664
Emilse Arias <emilsearias@gmail.com>
2023-03-27 18:36:02 -0300

    agregamos un ejemplo de herencia en nuestro prototipo pokemon

--- js/9-clases.js
+++ js/9-clases.js
@@ -1,28 +1,82 @@
 // ES6
 class Pokemon{
+    //se ejecuta automaticamente cuando creo un nuevo objeto
+    #tipo;// declaro la propiedad privada
     constructor(nombre, tipo, color){
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.color = color;
+        this._nombre = nombre;
+        this.#tipo = tipo;
+        this._color = color;
         this.vida = 100; //propiedad por defecto
     }
     //aqui van todos los metodos que necesite
     tirarPokebola(){
         document.write('<p>Salio de la pokebola, y aperecio un '+this.nombre +' </p>')
     }
-    // get y set
+    // get y set propiedades computadas
+    get color(){
+        return this._color;
+    }
 
-    // propiedades privadas
+    set color(nuevoColor){
+        if(nuevoColor.length > 0){
+            this._color = nuevoColor;
+        }
+    }
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nuevoNombre){
+        if(nuevoNombre.length > 0){
+            this._nombre = nuevoNombre;
+        }
+    }
+    get tipo(){
+        return this.#tipo;
+    }
+
+    set tipo(nuevoTipo){
+        if(nuevoTipo.length > 0){
+            this.#tipo = nuevoTipo;
+        }else{
+            console.log('el valor asignado de tipo es incorrecto');
+        }
+    }
 }
 
+class PokemonFuego extends Pokemon{
+    #ataque;
+    #debilidad;
+    constructor(nombre, tipo, color, ataque, debilidad){
+       super(nombre,tipo,color);//invoco al constructor del padre
+       this.#ataque = ataque;
+       this.#debilidad = debilidad;
+    }
+//get y set
+
+//metodos extras
+}
 
 // instanciar un prototipo
-const pokemonFuego = new Pokemon('Charmander', 'Fuego', 'Naranja');
+const pokemonCharmander = new Pokemon('Charmander', 'Fuego', 'Naranja');
 const pokemonPlanta = new Pokemon('Bulbasaur', 'Planta', 'Verde');
 
-console.log(pokemonFuego)
+console.log(pokemonCharmander)
 console.log(pokemonPlanta)
 
-pokemonFuego.tirarPokebola();
+pokemonCharmander.tirarPokebola();
 pokemonPlanta.tirarPokebola();
 
+pokemonCharmander.color = 'amarillo';
+document.write(`<p>Color del pokemon ${pokemonCharmander.nombre} es de color ${pokemonCharmander.color}</p>`)
+document.write(`<p>El pokemon ${pokemonCharmander.nombre} es de tipo ${pokemonCharmander.tipo}</p>`)
+
+pokemonCharmander.tipo = 'Electrico';
+document.write(`<p>El pokemon ${pokemonCharmander.nombre} es de tipo ${pokemonCharmander.tipo}</p>`)
+
+
+const charizar = new PokemonFuego('Charizar','Fuego', 'rojo','lanzallamas', 'agua');
+
+console.log(charizar);
+
+charizar.tirarPokebola();
