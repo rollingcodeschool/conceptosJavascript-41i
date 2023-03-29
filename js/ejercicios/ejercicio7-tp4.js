79050b26e28740b62a9d4046caf562113f10c710
Emilse Arias <emilsearias@gmail.com>
2023-03-29 18:39:43 -0300

    agregamos el metodo añadir contacto con una validacion

--- js/ejercicios/ejercicio7-tp4.js
+++ js/ejercicios/ejercicio7-tp4.js
@@ -24,14 +24,14 @@ class Contacto{
     }
 
     get nombre(){
-        this.#nombre;
+        return this.#nombre;
     }
 
     set nombre(nuevoNombre){
         this.#nombre = nuevoNombre
     }
     get telefono(){
-        this.#telefono;
+        return this.#telefono;
     }
 
     set telefono(nuevoTelefono){
@@ -48,21 +48,40 @@ class Agenda{
     }
 
     get contactos(){
-        this._contactos;
+        return this._contactos;
     }
 
     set contactos(nuevoContacto){
         this._contactos.push(nuevoContacto)
+      
     }
 
     get tamanio(){
-        this._tamanio;
+        return this._tamanio;
     }
 
     set tamanio(nuevoTamanio){
         this._tamanio = nuevoTamanio
     }
-    
+    aniadirContacto(contactoNuevo){
+        // verificar si el contactoNuevo ya existe en la agenda
+
+        //si la agenda tiene espacio disponible
+        if(!this.agendaLlena()){
+            this.contactos = contactoNuevo
+            console.log(this.contactos)
+        }
+    }
+
+    agendaLlena(){
+        if(this.contactos.length === this.tamanio){
+            console.log('agenda llena');
+            return true;
+        }else{
+            console.log('la agenda tiene espacio ');
+            return false;
+        }
+    }
     // aniadirContacto(Contacto): Añade un contacto a la agenda, sino la agenda no puede almacenar más contactos indicar por pantalla.
 // existeContacto(Contacto): indica si el contacto pasado existe o no.
 // listarContactos(): Lista toda la agenda
@@ -70,5 +89,57 @@ class Agenda{
 // eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
 // agendaLlena(): indica si la agenda está llena.
 // huecosLibres(): indica cuántos contactos más podemos ingresar.
-
 }
+
+let agendaNueva = new Agenda();
+console.log(agendaNueva);
+do{
+    const opcion = prompt(`Seleccione una de las siguientes opciones:
+    1- Añadir un contacto
+    2- Verificar si existe un contacto
+    3- Listar los contactos
+    4- Buscar un contacto
+    5- Eliminar un contacto
+    6- Verificar si la agenda esta llena
+    7- Verificar espacios disponibles en la agenda
+    8- Modificar el tamaño de la agenda`);
+    
+    switch(opcion){
+        case '1':
+            let nombre = prompt('Ingrese un nombre');
+            let telefono = prompt('Ingrese un telefono');
+            let contactoNuevo = new Contacto(nombre,telefono);
+            agendaNueva.aniadirContacto(contactoNuevo);
+            break;
+        case '2':
+            console.log('Verificar si existe un contacto');
+            break;
+        case '3':
+            console.log('1- Añadir un contacto');
+            break;
+        case '4':
+            console.log('1- Añadir un contacto');
+            break;
+        case '5':
+            console.log('1- Añadir un contacto');
+            break;
+        case '6':
+            console.log('1- Añadir un contacto');
+            break;
+        case '7':
+            console.log('1- Añadir un contacto');
+            break;
+        case '8':
+            console.log('1- Añadir un contacto');
+            break;
+        default:
+            alert('Ingresaste una opcion incorrecta')
+    
+    }
+}while(confirm('¿Desea realizar otra operacion?'))
+
+
+
+
+
+
